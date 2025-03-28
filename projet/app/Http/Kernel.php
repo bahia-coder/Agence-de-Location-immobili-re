e73102d4d65de7a1e5bd9621aa2ae39503d24e24
El bahia <126259077+bahia-coder@users.noreply.github.com>
2025-03-28 15:34:14 +0100

    Revert "inscription&login de user et admin"

--- projet/app/Http/Kernel.php
+++ projet/app/Http/Kernel.php
@@ -54,7 +54,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.check' => \App\Http\Middleware\CheckAuth::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
